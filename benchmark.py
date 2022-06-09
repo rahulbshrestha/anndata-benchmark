7dc1e470ee519bca29b3ba5b086582c87d95b3ea
rahulbshrestha <rahulshrestha0101@gmail.com>
2022-06-09 15:37:57 +0200

    upated benchmark

--- benchmark.py
+++ benchmark.py
@@ -9,14 +9,16 @@ import loompy as lp
 import h5py
 import scanpy as sc
 
+@profile
 def create_anndata_object():
     counts = csr_matrix(np.random.poisson(1, size=(2000, 2000)), dtype=np.float32)
     adata = ad.AnnData(counts)
 
-
+@profile
 def write_anndata_object():
-    
+    pass
 
+@profile
 def create_loom_object():
     filename = "test.loom"
     counts = csr_matrix(np.random.poisson(1, size=(2000,2000)), dtype=np.float32)
@@ -28,8 +30,8 @@ if __name__ == "__main__":
 
     # main()
     
-    # print(getrusage(RUSAGE_SELF))
-    %timeit create_anndata_object
-    #print(timeit.Timer(create_anndata_object).timeit(number=3))
-    #print(timeit.Timer(create_loom_object).timeit(number=3))
+    #print(getrusage(RUSAGE_SELF))
+    
+    print(timeit.Timer(create_anndata_object).timeit(number=3))
+    print(timeit.Timer(create_loom_object).timeit(number=3))
     
